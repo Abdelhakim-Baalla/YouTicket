ec693a5ebebbe6705de33d223a3d431ee5257d3e
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-23 12:17:06 +0100

    migration de la table clients

---
+++ database/migrations/2025_05_23_111508_create_clients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('compte')->constrained('utilisateurs');
+            $table->string('entreprise');
+            $table->foreignId('sla')->constrained('slas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+};
