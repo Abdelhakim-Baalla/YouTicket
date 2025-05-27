7a966ee203bdcb74e83779a3bf4397db95c0fa8f
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 17:58:31 +0100

    Ajout de la classe SlaRepository pour implémenter l'interface SlaRepositoryInterface.

---
+++ app/Repositories/SlaRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Sla;
+use App\Repositories\Interfaces\SlaRepositoryInterface;
+
+class SlaRepository implements SlaRepositoryInterface
+{
+    public function tous()
+    {
+
+    }
+    
+    public function trouver($id)
+    {
+
+    }
+    
+    public function creer(array $donnees)
+    {
+
+    }
+    
+    public function mettreAJour($id, array $donnees)
+    {
+
+    }
+    
+    public function supprimer($id)
+    {
+
+    }
+    
+}
