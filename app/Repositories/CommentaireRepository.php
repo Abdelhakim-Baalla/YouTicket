d742fd9f5ba99c8facf1da06bc769cea7921f1bd
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 17:48:18 +0100

    Ajout de la classe CommentaireRepository pour implémenter l'interface CommentaireRepositoryInterface.

---
+++ app/Repositories/CommentaireRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Commentaire;
+use App\Repositories\Interfaces\CommentaireRepositoryInterface;
+
+class CommentaireRepository implements CommentaireRepositoryInterface
+{
+    public function tous()
+    {
+
+    }
+    
+    public function trouver($id)
+    {
+
+    }
+    
+    public function creer(array $donnees)
+    {
+
+    }
+    
+    public function mettreAJour($id, array $donnees)
+    {
+
+    }
+    
+    public function supprimer($id)
+    {
+
+    }
+    
+}
