0fd1fa565680ab28acb6bffed896412f1824678a
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 17:53:41 +0100

    Ajout de la classe HistoriqueTicketRepository pour étendre la classe de base BaseRepository.

---
+++ app/Repositories/HistoriqueTicketRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repositories;
+
+use JasonGuru\LaravelMakeRepository\Repository\BaseRepository;
+//use Your Model
+
+/**
+ * Class HistoriqueTicketRepository.
+ */
+class HistoriqueTicketRepository extends BaseRepository
+{
+    /**
+     * @return string
+     *  Return the model
+     */
+    public function model()
+    {
+        //return YourModel::class;
+    }
+}
