367d126664ec8a03af2285f32863d2653903383a
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 17:49:11 +0100

    Ajout de la classe CompetenceRepository pour implémenter l'interface CompetenceRepositoryInterface.

---
+++ app/Repositories/CompetenceRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Competence;
+use App\Repositories\Interfaces\CompetenceRepositoryInterface;
+
+class CompetenceRepository implements CompetenceRepositoryInterface
+{
+    public function tous()
+    {
+
+    }
+    
+    public function trouver($id)
+    {
+
+    }
+    
+    public function creer(array $donnees)
+    {
+
+    }
+    
+    public function mettreAJour($id, array $donnees)
+    {
+
+    }
+    
+    public function supprimer($id)
+    {
+
+    }
+    
+}
