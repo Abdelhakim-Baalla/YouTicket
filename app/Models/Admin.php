d6a9f7877e2e9c3fd3123d0bf4ced0e25365b173
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 16:41:18 +0100

    Ajout des propriétés remplissables et de la relation utilisateur dans le modèle Admin

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Admin extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'utilisateur_id',
+        'permissions_speciales',
+    ];
+
+    public function utilisateur()
+    {
+        return $this->belongsTo(Utilisateur::class);
+    }
 }
