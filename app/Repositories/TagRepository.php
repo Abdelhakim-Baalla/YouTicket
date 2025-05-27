6ed331ac240b187a0b3c5e64c31d7c5c64af734e
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 17:59:06 +0100

    Ajout de la classe TagRepository pour implémenter l'interface TagRepositoryInterface.

---
+++ app/Repositories/TagRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Tag;
+use App\Repositories\Interfaces\TagRepositoryInterface;
+
+class TagRepository implements TagRepositoryInterface
+{
+    public function tous()
+    {
+
+    }
+    
+    public function trouver($id)
+    {
+
+    }
+    
+    public function creer(array $donnees)
+    {
+
+    }
+    
+    public function mettreAJour($id, array $donnees)
+    {
+
+    }
+    
+    public function supprimer($id)
+    {
+
+    }
+    
+}
