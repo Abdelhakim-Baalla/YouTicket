dd33532a966623f212c99493af51a61c2bc20342
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 15:25:42 +0100

    Ajout de nouveaux modèles pour la gestion des entités dans l'application YouTicket

---
+++ app/Models/TemplateTicket.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TemplateTicket extends Model
+{
+    use HasFactory;
+}
