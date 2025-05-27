85feb0875f568debf271f2668b4cce84b779333d
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 17:52:25 +0100

    Ajout de la classe FrequenceRepository pour implémenter l'interface FrequenceRepositoryInterface.

---
+++ app/Repositories/FrequenceRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Frequence;
+use App\Repositories\Interfaces\FrequenceRepositoryInterface;
+
+class FrequenceRepository implements FrequenceRepositoryInterface
+{
+    public function tous()
+    {
+
+    }
+    
+    public function trouver($id)
+    {
+
+    }
+    
+    public function creer(array $donnees)
+    {
+
+    }
+    
+    public function mettreAJour($id, array $donnees)
+    {
+
+    }
+    
+    public function supprimer($id)
+    {
+
+    }
+    
+}
