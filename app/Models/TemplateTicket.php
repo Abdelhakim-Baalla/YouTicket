4b2beb77b08344851afad3d8bd5aad4e0088db2d
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 16:44:02 +0100

    Ajout des propriétés remplissables et des relations dans le modèle TemplateTicket pour améliorer la gestion des templates de tickets dans l'application YouTicket.

--- app/Models/TemplateTicket.php
+++ app/Models/TemplateTicket.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class TemplateTicket extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nom',
+        'description',
+        'type_ticket_id',
+        'titre_template',
+        'description_template',
+        'champs_par_defaut',
+        'createur_id',
+        'actif',
+    ];
+
+    public function typeTicket()
+    {
+        return $this->belongsTo(TypeTicket::class);
+    }
+
+    public function createur()
+    {
+        return $this->belongsTo(Utilisateur::class, 'createur_id');
+    }
 }
