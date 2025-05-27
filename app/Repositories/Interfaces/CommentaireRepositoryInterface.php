1e86b6030b2834fb545e953cfb6ae3ee8b0748b2
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 17:22:32 +0100

    Ajout des interfaces de dépôt pour la gestion des entités dans l'application YouTicket.

---
+++ app/Repositories/Interfaces/CommentaireRepositoryInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+interface CommentaireRepositoryInterface
+{
+    public function tous();
+    public function trouver($id);
+    public function creer(array $donnees);
+    public function mettreAJour($id, array $donnees);
+    public function supprimer($id);
+}
---
+++ app/Repositories/Interfaces/ProjetRepositoryInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+interface ProjetRepositoryInterface
+{
+    public function tous();
+    public function trouver($id);
+    public function creer(array $donnees);
+    public function mettreAJour($id, array $donnees);
+    public function supprimer($id);
+}
