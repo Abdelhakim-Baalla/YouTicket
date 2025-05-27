a4c2c68826d715a3c342006d64ba024eb6448299
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 17:47:17 +0100

    Ajout de la classe CategorieKbRepository pour implémenter l'interface CategorieKbRepositoryInterface.

---
+++ app/Repositories/CategorieKbRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\CategorieKb;
+use App\Repositories\Interfaces\CategorieKbRepositoryInterface;
+
+class CategorieKbRepository implements CategorieKbRepositoryInterface
+{
+    public function tous()
+    {
+
+    }
+    
+    public function trouver($id)
+    {
+
+    }
+    
+    public function creer(array $donnees)
+    {
+
+    }
+    
+    public function mettreAJour($id, array $donnees)
+    {
+
+    }
+    
+    public function supprimer($id)
+    {
+
+    }
+    
+}
