5712bbe5faae9242101538a7e2a4b0e6353a3a3a
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 17:56:03 +0100

    Ajout de la classe PrioriteRepository pour implémenter l'interface PrioriteRepositoryInterface.

---
+++ app/Repositories/PrioriteRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Priorite;
+use App\Repositories\Interfaces\PrioriteRepositoryInterface;
+
+class PrioriteRepository implements PrioriteRepositoryInterface
+{
+    public function tous()
+    {
+
+    }
+    
+    public function trouver($id)
+    {
+
+    }
+    
+    public function creer(array $donnees)
+    {
+
+    }
+    
+    public function mettreAJour($id, array $donnees)
+    {
+
+    }
+    
+    public function supprimer($id)
+    {
+
+    }
+    
+}
