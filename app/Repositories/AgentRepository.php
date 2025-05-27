dc9a98383b124bae1bfec1f6d49a2748287f1d32
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 17:45:14 +0100

    Ajout de la classe AgentRepository pour implémenter l'interface AgentRepositoryInterface.

---
+++ app/Repositories/AgentRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Agent;
+use App\Repositories\Interfaces\AgentRepositoryInterface;
+
+class AgentRepository implements AgentRepositoryInterface
+{
+    public function tous()
+    {
+
+    }
+    
+    public function trouver($id)
+    {
+
+    }
+    
+    public function creer(array $donnees)
+    {
+
+    }
+    
+    public function mettreAJour($id, array $donnees)
+    {
+
+    }
+    
+    public function supprimer($id)
+    {
+
+    }
+    
+}
