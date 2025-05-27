b4ebf6f8ff12ec25a38e10bb92a30a411026409e
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 17:56:40 +0100

    Ajout de la classe ProjetRepository pour implémenter l'interface ProjetRepositoryInterface.

---
+++ app/Repositories/ProjetRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Projet;
+use App\Repositories\Interfaces\ProjetRepositoryInterface;
+
+class ProjetRepository implements ProjetRepositoryInterface
+{
+    public function tous()
+    {
+
+    }
+    
+    public function trouver($id)
+    {
+
+    }
+    
+    public function creer(array $donnees)
+    {
+
+    }
+    
+    public function mettreAJour($id, array $donnees)
+    {
+
+    }
+    
+    public function supprimer($id)
+    {
+
+    }
+    
+}
