015a7eeb2fe1c2d1fab82154e99b03fea067add3
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 16:43:39 +0100

    Ajout des propriétés remplissables et des relations dans les modèles Role et RolePermission pour améliorer la gestion des rôles et des permissions dans l'application YouTicket.

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nom',
+        'description',
+    ];
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class, 'role_permission');
+    }
 }
