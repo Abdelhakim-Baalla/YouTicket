dfa9ac4b2ec9238822581ba84163ee5fa8436817
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 17:50:08 +0100

    Ajout de la classe EquipeRepository pour implémenter l'interface EquipeRepositoryInterface.

---
+++ app/Repositories/EquipeRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Equipe;
+use App\Repositories\Interfaces\EquipeRepositoryInterface;
+
+class EquipeRepository implements EquipeRepositoryInterface
+{
+    public function tous()
+    {
+
+    }
+    
+    public function trouver($id)
+    {
+
+    }
+    
+    public function creer(array $donnees)
+    {
+
+    }
+    
+    public function mettreAJour($id, array $donnees)
+    {
+
+    }
+    
+    public function supprimer($id)
+    {
+
+    }
+    
+}
