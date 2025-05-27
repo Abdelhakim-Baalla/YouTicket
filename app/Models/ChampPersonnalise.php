33dfc9fcccf646ef1e562c2e806dbac9bc5b5e20
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 16:41:56 +0100

    Ajout des propriétés remplissables et de la relation typeTicket dans le modèle ChampPersonnalise

--- app/Models/ChampPersonnalise.php
+++ app/Models/ChampPersonnalise.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class ChampPersonnalise extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nom',
+        'libelle',
+        'type',
+        'options',
+        'obligatoire',
+        'ordre',
+        'type_ticket_id',
+    ];
+
+    public function typeTicket()
+    {
+        return $this->belongsTo(TypeTicket::class);
+    }
 }
