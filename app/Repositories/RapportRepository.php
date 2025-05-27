72e5e1e5659d7fb120f4c69078214f9f4677bc3d
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 17:57:13 +0100

    Ajout de la classe RapportRepository pour implémenter l'interface RapportRepositoryInterface.

---
+++ app/Repositories/RapportRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Rapport;
+use App\Repositories\Interfaces\RapportRepositoryInterface;
+
+class RapportRepository implements RapportRepositoryInterface
+{
+    public function tous()
+    {
+
+    }
+    
+    public function trouver($id)
+    {
+
+    }
+    
+    public function creer(array $donnees)
+    {
+
+    }
+    
+    public function mettreAJour($id, array $donnees)
+    {
+
+    }
+    
+    public function supprimer($id)
+    {
+
+    }
+    
+}
