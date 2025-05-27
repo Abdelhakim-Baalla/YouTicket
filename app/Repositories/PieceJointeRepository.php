9b502c0148545fac3775edd78a9acf2dd9630db9
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 17:55:27 +0100

    Ajout de la classe PieceJointeRepository pour implémenter l'interface PieceJointeRepositoryInterface.

---
+++ app/Repositories/PieceJointeRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\PieceJointe;
+use App\Repositories\Interfaces\PieceJointeRepositoryInterface;
+
+class PieceJointeRepository implements PieceJointeRepositoryInterface
+{
+    public function tous()
+    {
+
+    }
+    
+    public function trouver($id)
+    {
+
+    }
+    
+    public function creer(array $donnees)
+    {
+
+    }
+    
+    public function mettreAJour($id, array $donnees)
+    {
+
+    }
+    
+    public function supprimer($id)
+    {
+
+    }
+    
+}
