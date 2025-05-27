456b384c375d9b295f079c09bcd2d46566986360
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 17:43:09 +0100

    Ajout de la classe AdminRepository pour implémenter l'interface AdminRepositoryInterface.

---
+++ app/Repositories/AdminRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Admin;
+use App\Repositories\Interfaces\AdminRepositoryInterface;
+
+class AdminRepository implements AdminRepositoryInterface
+{
+    public function tous()
+    {
+
+    }
+    
+    public function trouver($id)
+    {
+
+    }
+    
+    public function creer(array $donnees)
+    {
+
+    }
+    
+    public function mettreAJour($id, array $donnees)
+    {
+
+    }
+    
+    public function supprimer($id)
+    {
+
+    }
+    
+}
