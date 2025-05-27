470c3c0c750650ffbab7d502963503feb775c102
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 17:57:48 +0100

    Ajout de la classe RegleEscaladeRepository pour implémenter l'interface RegleEscaladeRepositoryInterface.

---
+++ app/Repositories/RegleEscaladeRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\RegleEscalade;
+use App\Repositories\Interfaces\RegleEscaladeRepositoryInterface;
+
+class RegleEscaladeRepository implements RegleEscaladeRepositoryInterface
+{
+    public function tous()
+    {
+
+    }
+    
+    public function trouver($id)
+    {
+
+    }
+    
+    public function creer(array $donnees)
+    {
+
+    }
+    
+    public function mettreAJour($id, array $donnees)
+    {
+
+    }
+    
+    public function supprimer($id)
+    {
+
+    }
+    
+}
