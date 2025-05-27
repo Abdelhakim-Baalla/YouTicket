4502235eae830d2a9e8d9d4f9bc0625acde55e98
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 17:46:12 +0100

    Ajout de la classe BaseConnaissanceRepository pour implémenter l'interface BaseConnaissanceRepositoryInterface.

---
+++ app/Repositories/BaseConnaissanceRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\BaseConnaissance;
+use App\Repositories\Interfaces\BaseConnaissanceRepositoryInterface;
+
+class BaseConnaissanceRepository implements BaseConnaissanceRepositoryInterface
+{
+    public function tous()
+    {
+
+    }
+    
+    public function trouver($id)
+    {
+
+    }
+    
+    public function creer(array $donnees)
+    {
+
+    }
+    
+    public function mettreAJour($id, array $donnees)
+    {
+
+    }
+    
+    public function supprimer($id)
+    {
+
+    }
+    
+}
