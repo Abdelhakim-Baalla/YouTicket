92eafc4ce8afb116579f3dce8ae9b5d3a73f1897
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 18:00:04 +0100

    Ajout de la classe TicketRepository pour implémenter l'interface TicketRepositoryInterface.

---
+++ app/Repositories/TicketRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Ticket;
+use App\Repositories\Interfaces\TicketRepositoryInterface;
+
+class TicketRepository implements TicketRepositoryInterface
+{
+    public function tous()
+    {
+
+    }
+    
+    public function trouver($id)
+    {
+
+    }
+    
+    public function creer(array $donnees)
+    {
+
+    }
+    
+    public function mettreAJour($id, array $donnees)
+    {
+
+    }
+    
+    public function supprimer($id)
+    {
+
+    }
+    
+}
