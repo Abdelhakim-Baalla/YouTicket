c41bfc29b5f870fe4529ea28920273ff47d88235
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 17:54:46 +0100

    Ajout de la classe NotificationRepository pour implémenter l'interface NotificationRepositoryInterface.

---
+++ app/Repositories/NotificationRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Notification;
+use App\Repositories\Interfaces\NotificationRepositoryInterface;
+
+class NotificationRepository implements NotificationRepositoryInterface
+{
+    public function tous()
+    {
+
+    }
+    
+    public function trouver($id)
+    {
+
+    }
+    
+    public function creer(array $donnees)
+    {
+
+    }
+    
+    public function mettreAJour($id, array $donnees)
+    {
+
+    }
+    
+    public function supprimer($id)
+    {
+
+    }
+    
+}
