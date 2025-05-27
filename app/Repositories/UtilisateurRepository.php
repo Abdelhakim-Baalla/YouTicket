ffd3326d342962b776db505c38b475d81ff3f2e3
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-27 18:00:44 +0100

    Ajout de la classe UtilisateurRepository pour implémenter l'interface UtilisateurRepositoryInterface.

---
+++ app/Repositories/UtilisateurRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Utilisateur;
+use App\Repositories\Interfaces\UtilisateurRepositoryInterface;
+
+class UtilisateurRepository implements UtilisateurRepositoryInterface
+{
+    public function tous()
+    {
+
+    }
+    
+    public function trouver($id)
+    {
+
+    }
+    
+    public function creer(array $donnees)
+    {
+
+    }
+    
+    public function mettreAJour($id, array $donnees)
+    {
+
+    }
+    
+    public function supprimer($id)
+    {
+
+    }
+    
+}
