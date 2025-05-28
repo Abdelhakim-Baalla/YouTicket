3aeebcfdcb00e7348d26198dae2b14e1d5c52010
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-28 10:15:16 +0100

    Refactor de PieceJointeRepository pour améliorer la lisibilité du code en supprimant les lignes vides inutiles.

--- app/Repositories/PieceJointeRepository.php
+++ app/Repositories/PieceJointeRepository.php
@@ -9,27 +9,41 @@ class PieceJointeRepository implements PieceJointeRepositoryInterface
 {
     public function tous()
     {
-
+        return PieceJointe::all();
     }
     
     public function trouver($id)
     {
+        return PieceJointe::find($id);
 
     }
     
     public function creer(array $donnees)
     {
+        return PieceJointe::create($donnees);
 
     }
     
     public function mettreAJour($id, array $donnees)
     {
+        $pieceJointe = $this->trouver($id);
 
+        if($pieceJointe)
+        {
+            return $pieceJointe->update($donnees);
+        }
+        return null;
     }
     
     public function supprimer($id)
     {
+        $pieceJointe = $this->trouver($id);
 
+        if($pieceJointe)
+        {
+            return $pieceJointe->delete();
+        }
+        return false;
     }
     
 }
