698b9da1a9f71e9a54168dea74eaf7fe608aeee0
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-28 09:49:02 +0100

    Refactor de BaseConnaissanceRepository pour améliorer la lisibilité du code en supprimant les lignes vides inutiles.

--- app/Repositories/BaseConnaissanceRepository.php
+++ app/Repositories/BaseConnaissanceRepository.php
@@ -9,27 +9,39 @@ class BaseConnaissanceRepository implements BaseConnaissanceRepositoryInterface
 {
     public function tous()
     {
-
+        return BaseConnaissance::all();
     }
     
     public function trouver($id)
     {
-
+        return BaseConnaissance::find($id);
     }
     
     public function creer(array $donnees)
     {
-
+        return BaseConnaissance::create($donnees);
     }
     
     public function mettreAJour($id, array $donnees)
     {
-
+        $baseConnaissance = $this->trouver($id);
+        if($baseConnaissance)
+        {
+            $baseConnaissance->update($donnees);
+            return $baseConnaissance;
+        }
+        return null;
     }
     
     public function supprimer($id)
     {
-
+        $baseConnaissance = $this->trouver($id);
+        if($baseConnaissance)
+        {
+            $baseConnaissance->delete();
+            return true;
+        }
+        return false;
     }
     
 }
