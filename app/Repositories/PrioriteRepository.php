bf195abdc3869eeacd5b46f8dfa1912dcba28aef
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-28 10:17:24 +0100

    Refactor de PrioriteRepository pour améliorer la lisibilité du code en supprimant les lignes vides inutiles.

--- app/Repositories/PrioriteRepository.php
+++ app/Repositories/PrioriteRepository.php
@@ -9,27 +9,37 @@ class PrioriteRepository implements PrioriteRepositoryInterface
 {
     public function tous()
     {
-
+        return Priorite::all();
     }
     
     public function trouver($id)
     {
-
+        return Priorite::find($id);
     }
     
     public function creer(array $donnees)
     {
-
+        return Priorite::create($donnees);
     }
     
     public function mettreAJour($id, array $donnees)
     {
+        $priorite = $this->trouver($id);
 
+        if ($priorite) {
+            return $priorite->update($donnees);
+        }
+        return null;
     }
     
     public function supprimer($id)
     {
+        $priorite = $this->trouver($id);
 
+        if ($priorite) {
+            return $priorite->delete();
+        }
+        return false;
     }
     
 }
