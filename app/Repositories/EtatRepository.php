e8dcd105b44051daad76664ee49850461b2b9a05
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-28 10:04:58 +0100

    Refactor de EtatRepository pour améliorer la lisibilité du code en supprimant les lignes vides inutiles.

--- app/Repositories/EtatRepository.php
+++ app/Repositories/EtatRepository.php
@@ -9,27 +9,39 @@ class EtatRepository implements EtatRepositoryInterface
 {
     public function tous()
     {
-
+        return Etat::all();
     }
     
     public function trouver($id)
     {
-
+        return Etat::find($id);
     }
     
     public function creer(array $donnees)
     {
-
+        return Etat::create($donnees);
     }
     
     public function mettreAJour($id, array $donnees)
     {
+        $etat = $this->trouver($id);
 
+        if($etat)
+        {
+            return $etat->update($donnees);
+        }
+        return null;
     }
     
     public function supprimer($id)
     {
+        $etat = $this->trouver($id);
 
+        if($etat)
+        {
+            return $etat->delete();
+        }
+        return false;
     }
     
 }
