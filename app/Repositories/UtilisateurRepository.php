73a3436d63722331c909c5c8257945dc0083393e
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-28 10:25:41 +0100

    Refactor de UtilisateurRepository pour améliorer la lisibilité du code en supprimant les lignes vides inutiles.

--- app/Repositories/UtilisateurRepository.php
+++ app/Repositories/UtilisateurRepository.php
@@ -9,27 +9,39 @@ class UtilisateurRepository implements UtilisateurRepositoryInterface
 {
     public function tous()
     {
-
+        return Utilisateur::all();
     }
     
     public function trouver($id)
     {
-
+        return Utilisateur::find($id);
     }
     
     public function creer(array $donnees)
     {
-
+        return Utilisateur::create($donnees);
     }
     
     public function mettreAJour($id, array $donnees)
     {
-
+        $utilisateur = $this->trouver($id);
+        
+        if ($utilisateur) 
+        {
+            return $utilisateur->update($donnees);
+        }
+        return null;
     }
     
     public function supprimer($id)
     {
-
+        $utilisateur = $this->trouver($id);
+        
+        if ($utilisateur) 
+        {
+            return $utilisateur->delete();
+        }
+        return false;
     }
     
 }
