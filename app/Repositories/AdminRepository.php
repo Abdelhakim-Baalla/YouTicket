087dd6200f032a6469246311a0cfc6c78441c2af
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-28 09:43:52 +0100

    Refactor de l'AdminRepository pour améliorer la lisibilité du code en supprimant les lignes vides inutiles.

--- app/Repositories/AdminRepository.php
+++ app/Repositories/AdminRepository.php
@@ -9,27 +9,38 @@ class AdminRepository implements AdminRepositoryInterface
 {
     public function tous()
     {
-
+        return Admin::all();
     }
-    
+
     public function trouver($id)
     {
-
+        return Admin::find($id);
     }
-    
+
     public function creer(array $donnees)
     {
-
+        return Admin::create($donnees);
     }
-    
+
     public function mettreAJour($id, array $donnees)
     {
-
+        $admin = $this->trouver($id);
+        if ($admin) 
+        {
+            $admin->update($donnees);
+            return $admin;
+        }
+        return null;
     }
-    
+
     public function supprimer($id)
     {
-
+        $admin = $this->trouver($id);
+        if($admin)
+        {
+            $admin->delete();
+            return true;
+        }
     }
-    
+
 }
