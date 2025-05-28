ce804e3e467e4fd0ab7baca98d0a70503417d59b
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-28 10:23:27 +0100

    Refactor de SlaRepository pour améliorer la lisibilité du code en supprimant les lignes vides inutiles.

--- app/Repositories/SlaRepository.php
+++ app/Repositories/SlaRepository.php
@@ -9,27 +9,39 @@ class SlaRepository implements SlaRepositoryInterface
 {
     public function tous()
     {
-
+        return Sla::all();
     }
     
     public function trouver($id)
     {
-
+        return Sla::find($id);
     }
     
     public function creer(array $donnees)
     {
-
+        return Sla::create($donnees);
     }
     
     public function mettreAJour($id, array $donnees)
     {
+        $sla = $this->trouver($id);
 
+        if ($sla) 
+        {
+            return $sla->update($donnees);
+        }
+        return null;
     }
     
     public function supprimer($id)
     {
+        $sla = $this->trouver($id);
 
+        if ($sla) 
+        {
+            return $sla->delete();
+        }
+        return false;
     }
     
 }
