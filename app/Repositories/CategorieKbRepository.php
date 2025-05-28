3248c4f93c11139f88c237a92422a2197edd685a
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-28 09:51:35 +0100

    Refactor de CategorieKbRepository pour améliorer la lisibilité du code en supprimant les lignes vides inutiles.

--- app/Repositories/CategorieKbRepository.php
+++ app/Repositories/CategorieKbRepository.php
@@ -9,27 +9,37 @@ class CategorieKbRepository implements CategorieKbRepositoryInterface
 {
     public function tous()
     {
-
+        return CategorieKb::all();
     }
     
     public function trouver($id)
     {
-
+        return CategorieKb::find($id);
     }
     
     public function creer(array $donnees)
     {
-
+        return CategorieKb::create($donnees);
     }
     
     public function mettreAJour($id, array $donnees)
     {
-
+        $categorie = $this->trouver($id);
+        if ($categorie) {
+            $categorie->update($donnees);
+            return $categorie;
+        }
+        return null;
     }
     
     public function supprimer($id)
     {
-
+        $categorie = $this->trouver($id);
+        if ($categorie) {
+            $categorie->delete();
+            return true;
+        }
+        return false;
     }
     
 }
