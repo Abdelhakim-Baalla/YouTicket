76319ed5cfc426b7a04e5b8992799d6212814322
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-28 10:10:25 +0100

    Refactor de HistoriqueTicketRepository pour améliorer la lisibilité du code en supprimant les lignes vides inutiles.

--- app/Repositories/HistoriqueTicketRepository.php
+++ app/Repositories/HistoriqueTicketRepository.php
@@ -9,27 +9,40 @@ class HistoriqueTicketRepository implements HistoriqueTicketRepositoryInterface
 {
     public function tous()
     {
-
+        return HistoriqueTicket::all();
     }
     
     public function trouver($id)
     {
-
+        return HistoriqueTicket::find($id);
     }
     
     public function creer(array $donnees)
     {
-
+        return HistoriqueTicket::create($donnees);
     }
     
     public function mettreAJour($id, array $donnees)
     {
+        $historiqueTicket = $this->trouver($id);
 
+        if ($historiqueTicket) 
+        {
+            $historiqueTicket->update($donnees);
+            return $historiqueTicket;
+        }
+        return null;
     }
     
     public function supprimer($id)
     {
+        $historiqueTicket = $this->trouver($id);
 
+        if ($historiqueTicket) 
+        {
+            return $historiqueTicket->delete();
+        }
+        return false;
     }
     
 }
