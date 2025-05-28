4ded39686dde5c286aa1d374100c309a1f6a9b53
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-28 10:22:13 +0100

    Refactor de RegleEscaladeRepository pour améliorer la lisibilité du code en supprimant les lignes vides inutiles.

--- app/Repositories/RegleEscaladeRepository.php
+++ app/Repositories/RegleEscaladeRepository.php
@@ -9,27 +9,39 @@ class RegleEscaladeRepository implements RegleEscaladeRepositoryInterface
 {
     public function tous()
     {
-
+        return RegleEscalade::all();
     }
     
     public function trouver($id)
     {
-
+        return RegleEscalade::find($id);
     }
     
     public function creer(array $donnees)
     {
-
+        return RegleEscalade::create($donnees);
     }
     
     public function mettreAJour($id, array $donnees)
     {
+        $regleEscalade = $this->trouver($id);
 
+        if ($regleEscalade) 
+        {
+            return $regleEscalade->update($donnees);
+        }
+        return null;
     }
     
     public function supprimer($id)
     {
+        $regleEscalade = $this->trouver($id);
 
+        if ($regleEscalade) 
+        {
+            return $regleEscalade->delete();
+        }
+        return false;
     }
     
 }
