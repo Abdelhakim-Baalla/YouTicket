3cfea23fd2aa4fa12c3a17ceb6483700dcad4177
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-28 09:56:23 +0100

    Refactor de CompetenceRepository pour améliorer la lisibilité du code en supprimant les lignes vides inutiles.

--- app/Repositories/CompetenceRepository.php
+++ app/Repositories/CompetenceRepository.php
@@ -9,27 +9,39 @@ class CompetenceRepository implements CompetenceRepositoryInterface
 {
     public function tous()
     {
-
+        return Competence::all();
     }
     
     public function trouver($id)
     {
-
+        return Competence::find($id);
     }
     
     public function creer(array $donnees)
     {
-
+        return Competence::create($donnees);
     }
     
     public function mettreAJour($id, array $donnees)
     {
-
+        $competence = $this->trouver($id);
+        if($competence)
+        {
+            $competence->update($donnees);
+            return $competence;
+        }
+        return null;
     }
     
     public function supprimer($id)
     {
-
+        $competence = $this->trouver($id);
+        if($competence)
+        {
+            $competence->delete();
+            return true;
+        }
+        return false;
     }
     
 }
