486ed9d1bb61ff9e70c02cfae087acc1befdadff
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-28 09:53:30 +0100

    Refactor de CommentaireRepository pour améliorer la lisibilité du code en supprimant les lignes vides inutiles.

--- app/Repositories/CommentaireRepository.php
+++ app/Repositories/CommentaireRepository.php
@@ -9,27 +9,37 @@ class CommentaireRepository implements CommentaireRepositoryInterface
 {
     public function tous()
     {
-
+        return Commentaire::all();
     }
     
     public function trouver($id)
     {
-
+        return Commentaire::find($id);
     }
     
     public function creer(array $donnees)
     {
-
+        return Commentaire::create($donnees);
     }
     
     public function mettreAJour($id, array $donnees)
     {
-
+        $commentaire = $this->trouver($id);
+        if ($commentaire) {
+            $commentaire->update($donnees);
+            return $commentaire;
+        }
+        return null;
     }
     
     public function supprimer($id)
     {
-
+        $commentaire = $this->trouver($id);
+        if ($commentaire) {
+            $commentaire->delete();
+            return true;
+        }
+        return false;
     }
     
 }
