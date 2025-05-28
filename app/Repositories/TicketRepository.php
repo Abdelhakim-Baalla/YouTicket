42eccc157563a27efcdf4b860381dd9ec2c0d491
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-28 10:24:32 +0100

    Refactor de TicketRepository pour améliorer la lisibilité du code en supprimant les lignes vides inutiles.

--- app/Repositories/TicketRepository.php
+++ app/Repositories/TicketRepository.php
@@ -9,27 +9,39 @@ class TicketRepository implements TicketRepositoryInterface
 {
     public function tous()
     {
-
+        return Ticket::all();
     }
     
     public function trouver($id)
     {
-
+        return Ticket::find($id);
     }
     
     public function creer(array $donnees)
     {
-
+        return Ticket::create($donnees);
     }
     
     public function mettreAJour($id, array $donnees)
     {
+        $ticket = $this->trouver($id);
 
+        if ($ticket) 
+        {
+            return $ticket->update($donnees);
+        }
+        return null;
     }
     
     public function supprimer($id)
     {
+        $ticket = $this->trouver($id);
 
+        if ($ticket) 
+        {
+            return $ticket->delete();
+        }
+        return false;
     }
     
 }
