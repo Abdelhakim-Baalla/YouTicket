65dc63937399b9ede56a09c2762545e6af9fe983
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-28 09:45:51 +0100

    Refactor de l'AgentRepository pour améliorer la lisibilité du code en supprimant les lignes vides inutiles.

--- app/Repositories/AgentRepository.php
+++ app/Repositories/AgentRepository.php
@@ -9,27 +9,37 @@ class AgentRepository implements AgentRepositoryInterface
 {
     public function tous()
     {
-
+        return Agent::all();
     }
     
     public function trouver($id)
     {
-
+        return Agent::find($id);
     }
     
     public function creer(array $donnees)
     {
-
+        return Agent::create($donnees);
     }
     
     public function mettreAJour($id, array $donnees)
     {
-
+        $agent = $this->trouver($id);
+        if ($agent) {
+            $agent->update($donnees);
+            return $agent;
+        }
+        return null;
     }
     
     public function supprimer($id)
     {
-
+        $agent = $this->trouver($id);
+        if ($agent) {
+            $agent->delete();
+            return true;
+        }
+        return false;
     }
     
 }
