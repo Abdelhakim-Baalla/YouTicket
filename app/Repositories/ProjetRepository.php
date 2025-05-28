5e2250435faee1e2e2ee619e90be4d23a8d1ddeb
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-28 10:19:52 +0100

    Refactor de ProjetRepository pour améliorer la lisibilité du code en supprimant les lignes vides inutiles.

--- app/Repositories/ProjetRepository.php
+++ app/Repositories/ProjetRepository.php
@@ -9,27 +9,39 @@ class ProjetRepository implements ProjetRepositoryInterface
 {
     public function tous()
     {
-
+        return Projet::all();
     }
     
     public function trouver($id)
     {
-
+        return Projet::find($id);
     }
     
     public function creer(array $donnees)
     {
-
+        return Projet::create($donnees);
     }
     
     public function mettreAJour($id, array $donnees)
     {
+        $projet = $this->trouver($id);;
 
+        if ($projet) 
+        {
+            return $projet->update($donnees); 
+        }
+        return null;
     }
     
     public function supprimer($id)
     {
+        $projet = $this->trouver($id);
 
+        if($projet)
+        {
+            return $projet->delete();
+        }
+        return false;
     }
     
 }
