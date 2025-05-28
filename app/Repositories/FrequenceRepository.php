d1a7c2198ae009bb519e6c374dced5cab848998d
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-05-28 10:08:37 +0100

    Refactor de FrequenceRepository pour améliorer la lisibilité du code en supprimant les lignes vides inutiles.

--- app/Repositories/FrequenceRepository.php
+++ app/Repositories/FrequenceRepository.php
@@ -9,27 +9,39 @@ class FrequenceRepository implements FrequenceRepositoryInterface
 {
     public function tous()
     {
-
+        return Frequence::all();
     }
     
     public function trouver($id)
     {
-
+        return Frequence::find($id);
     }
     
     public function creer(array $donnees)
     {
-
+        return Frequence::create($donnees);
     }
     
     public function mettreAJour($id, array $donnees)
     {
+        $frequence = $this->trouver($id);
 
+        if($frequence)
+        {
+            return $frequence->update($donnees);
+        }
+        return null;
     }
     
     public function supprimer($id)
     {
+        $frequence = $this->trouver($id);
 
+        if($frequence)
+        {
+            return $frequence->delete();
+        }
+        return false;
     }
     
 }
