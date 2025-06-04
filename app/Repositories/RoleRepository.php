be101f89bd10cddc681e950f75fbd1a6b99238bd
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-06-04 11:52:00 +0100

    Ajout de l'interface RoleRepositoryInterface et de l'implémentation RoleRepository pour la gestion des rôles

---
+++ app/Repositories/RoleRepository.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Role;
+use App\Repositories\Interfaces\RoleRepositoryInterface;
+
+class RoleRepository implements RoleRepositoryInterface
+{
+    public function tous()
+    {
+        return Role::all();
+    }
+    
+    public function trouver($id)
+    {
+        return Role::find($id);
+    }
+
+    public function trouverAvecNom($id, $nom)
+    {
+        return Role::where('nom', $nom)->get($id);
+    }
+    
+    public function creer(array $donnees)
+    {
+        return Role::create($donnees);
+    }
+    
+    public function mettreAJour($id, array $donnees)
+    {
+        $role = $this->trouver($id);
+
+        if ($role) 
+        {
+            return $role->update($donnees);
+        }
+        return null;
+    }
+    
+    public function supprimer($id)
+    {
+        $role = $this->trouver($id);
+
+        if ($role) 
+        {
+            return $role->delete();
+        }
+        return false;
+    }
+    
+}
