cfdae3439e169a20f90c2519cf8d5c877ffc2beb
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-06-12 14:39:02 +0100

    Ajouter le cas de si il ya un neavauex utilisateur registrer dans notre platforme et enregistra dans le log la photo il n'exist pas car il ne enregistrer pas di le debut !!

--- app/Http/Controllers/UtilisateurController.php
+++ app/Http/Controllers/UtilisateurController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\Interfaces\TicketRepositoryInterface;
 use Illuminate\Http\Request;
 
 class UtilisateurController extends Controller
 {
+    protected $ticketRepository;
+
+    public function __construct(TicketRepositoryInterface $ticketRepository)
+    {
+        $this->ticketRepository = $ticketRepository;
+    }
+
      public function showUtilisateurDashboard()
     {
+        $countTicketActif= $this->ticketRepository->tous()->count();
+        dd($countTicketActif);
         return view('dashboard.utilisateur.index');
     }
 }
