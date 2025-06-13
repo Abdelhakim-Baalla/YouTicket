b3248085e68ad8e718fccbddeaaa0454ef800960
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-06-13 10:19:47 +0100

    Ajouter l'icon de l'application

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="{{ asset('images/YouTicketLogo.png') }}" type="image/x-icon">
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <style>
         :root {
