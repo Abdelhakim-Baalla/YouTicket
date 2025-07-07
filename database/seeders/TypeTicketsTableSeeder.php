03fcc8910d671420d60cd42fdd62a98d0083dc4e
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-07-07 11:24:17 +0100

    Ajout du seeder TypeTicketsTableSeeder pour initialiser les types de tickets dans la base de données.

---
+++ database/seeders/TypeTicketsTableSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TypeTicketsTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('type_tickets')->insert([
+            [
+                'id' => 1,
+                'nom' => 'Incident',
+                'description' => 'Problème technique à résoudre',
+                'icone' => 'bug_report',
+                'actif' => 1,
+                'created_at' => '2025-06-03 11:29:22',
+                'updated_at' => '2025-06-03 11:29:22',
+            ],
+            [
+                'id' => 2,
+                'nom' => 'Demande',
+                'description' => 'Demande de service ou fonctionnalité',
+                'icone' => 'help_outline',
+                'actif' => 1,
+                'created_at' => '2025-06-03 11:29:22',
+                'updated_at' => '2025-06-03 11:29:22',
+            ],
+            [
+                'id' => 3,
+                'nom' => 'Problème',
+                'description' => 'Problème nécessitant une investigation',
+                'icone' => 'warning',
+                'actif' => 1,
+                'created_at' => '2025-06-03 11:29:22',
+                'updated_at' => '2025-06-03 11:29:22',
+            ],
+            [
+                'id' => 4,
+                'nom' => 'Question',
+                'description' => 'Question ou demande d\'information',
+                'icone' => 'help',
+                'actif' => 1,
+                'created_at' => '2025-06-03 11:29:22',
+                'updated_at' => '2025-06-03 11:29:22',
+            ],
+        ]);
+    }
+}
