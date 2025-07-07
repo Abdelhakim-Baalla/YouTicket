c3753e6c69c8121c141eecb55e4119b9698a8494
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-07-07 11:16:31 +0100

    Suppression des commentaires de documentation et nettoyage du code dans DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,18 +2,27 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            EtatsTableSeeder::class,
+            PrioritesTableSeeder::class,
+            FrequencesTableSeeder::class,
+            TypeTicketsTableSeeder::class,
+            RolesTableSeeder::class,
+            HorairesTravailTableSeeder::class,
+            ProjetsTableSeeder::class,
+            SlasTableSeeder::class,
+            EquipesTableSeeder::class,
+            AgentsTableSeeder::class,
+            TicketsTableSeeder::class,
+            CommentairesTableSeeder::class,
+            PieceJointesTableSeeder::class,
+            NotificationsTableSeeder::class,
+        ]);
     }
 }
