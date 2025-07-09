d994335f998309a012a8225ef1b96f1e73230bd1
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-07-09 11:41:00 +0100

    Nettwoyer le code et ajouter des commentaires dans le modèle Notification

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -20,11 +20,13 @@ class Notification extends Model
         'date_envoi',
     ];
 
+    // Définition de la relation entre le modèle Notification et Utilisateur
     public function utilisateur()
     {
         return $this->belongsTo(Utilisateur::class);
     }
 
+    // Définition de la relation entre le modèle Notification et Ticket
     public function ticket()
     {
         return $this->belongsTo(Ticket::class, 'ticket_id');
