f5470765cfee141dda4312ff07f0b460944551a3
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-07-09 11:35:24 +0100

    Nettwoyer le code et ajouter des commentaires dans le modèle Commentaire

--- app/Models/Commentaire.php
+++ app/Models/Commentaire.php
@@ -17,11 +17,13 @@ class Commentaire extends Model
         'interne',
     ];
 
+    // Définition des relations entre le modèle Commentaire et Ticket
     public function ticket()
     {
         return $this->belongsTo(Ticket::class);
     }
 
+    // Définition des relations entre le modèle Commentaire et Utilisateur
     public function utilisateur()
     {
         return $this->belongsTo(Utilisateur::class);
